101e721097e4739e516bcf9973837cfbabacdfad
“Nayonna <Nayonna Purnell>
2023-02-01 22:55:47 -0500

    Added the footer file and updated the file structure

--- nayonna-purnell-portfolio/src/components/Footer.js
+++ nayonna-purnell-portfolio/src/components/Footer.js
@@ -1,10 +1,50 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 function Footer() {
     return (
-        <footer>Hi I am in the footer</footer>
-    )
+        <div className="card border-0 sticky-bottom">
+        <div className="container">
+        <div class="row">
+          <div class="col-sm">
+          <Form>
+            <h2>Let's Work Together!!!</h2>
+        <Form.Group className="mb-2" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
 
+      <Form.Group className="mb-2" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+          </div>
+          <div class="col-sm">
+            <ul className='list-unstyled'>
+                <li>Home</li>
+                <li>About Me</li>
+                <li>Projects</li>
+            </ul>
+          </div>
+          <div class="col-sm">
+          <ul className='list-unstyled'>
+             <li>GitHub</li>
+             <li>LinkedIn</li>
+             <li>Email</li>
+         </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    )
 }
 
 export default Footer;
--- nayonna-purnell-portfolio/src/components/Project.js
+++ nayonna-purnell-portfolio/src/components/Project.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Footer from "./Footer";
 
 function Project() {
     return (
+        <div>
          <h1>Hi I am in the temporary Project</h1>
-       
+       <Footer />
+       </div>
     )
-
 }
 
 export default Project;
